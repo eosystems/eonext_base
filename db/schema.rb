15a60b4117d72cc037fcc3682b4883e3099ae9d8
r.nishio <quick.xp@gmail.com>
2019-03-23 16:27:30 +0900

    dev

---
+++ db/schema.rb
@@ -0,0 +1,59 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 0) do
+
+  create_table "delayed_jobs", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT", comment: "Delayed Job", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at", comment: "作成日時"
+    t.datetime "updated_at", comment: "更新日時"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  end
+
+  create_table "user_details", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "corporation_id"
+    t.integer "alliance_id"
+    t.string "key_id"
+    t.string "verification_code"
+  end
+
+  create_table "users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT", force: :cascade do |t|
+    t.string "uid", null: false
+    t.string "name"
+    t.string "token"
+    t.string "refresh_token"
+    t.datetime "expire"
+    t.string "encrypted_password", default: "", null: false
+    t.string "provider", null: false
+    t.string "email", default: ""
+    t.string "reset_password_token", default: ""
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip", default: "", null: false
+    t.string "last_sign_in_ip", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
---
+++ db/schema/schema.rb
@@ -0,0 +1,60 @@
+create_table "schema_migrations", collate: "utf8_bin", comment: "" do |t|
+  t.varchar "version"
+
+  t.index "version", name: "unique_schema_migrations", unique: true
+end
+
+create_table :users, collate: "utf8_bin" do |t|
+  t.int :id, primary_key: true, extra: :auto_increment
+  t.varchar :uid
+  t.varchar :name, null: true
+  t.varchar :token, null:true
+  t.varchar :refresh_token, null:true
+  t.datetime :expire, null:true
+  t.varchar :encrypted_password, null: false, default: ""
+  t.varchar :provider
+  t.varchar :email,null: true, default: ""
+
+  ## Recoverable
+  t.varchar   :reset_password_token, null: true, default: ""
+  t.datetime :reset_password_sent_at, null: true
+
+  ## Rememberable
+  t.datetime :remember_created_at, null: true
+
+  ## Trackable
+  t.int  :sign_in_count, default: 0, null: false
+  t.datetime :current_sign_in_at, null: true
+  t.datetime :last_sign_in_at, null: true
+  t.varchar   :current_sign_in_ip, default: ""
+  t.varchar   :last_sign_in_ip, default:""
+  t.datetime :created_at
+  t.datetime :updated_at
+end
+
+create_table :user_details, collate: "utf8_bin" do |t|
+  t.int :id, primary_key: true, extra: :auto_increment
+  t.int :user_id
+  t.int :corporation_id, null: true
+  t.int :alliance_id, null: true
+  t.varchar :key_id, null: true
+  t.varchar :verification_code, null: true
+end
+
+create_table :delayed_jobs, comment: 'Delayed Job' do |t|
+  t.int :id, primary_key: true, extra: 'auto_increment'
+  t.int :priority, default: 0, null: false
+  t.int :attempts, default: 0, null: false
+  t.text :handler
+  t.text :last_error, null: true
+  t.datetime :run_at, null: true
+  t.datetime :locked_at, null: true
+  t.datetime :failed_at, null: true
+  t.varchar :locked_by, null: true
+  t.varchar :queue, null: true
+
+  t.datetime :created_at, null: true, comment: '作成日時'
+  t.datetime :updated_at, null: true, comment: '更新日時'
+
+  t.index [:priority, :run_at], name: 'delayed_jobs_priority'
+end
