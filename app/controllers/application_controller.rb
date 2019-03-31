65f854787a278a7e9291c289ee458ce516610661
r.nishio <quick.xp@gmail.com>
2019-03-31 21:05:51 +0900

    dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    token = request.headers[:EONEXT_LOGIN_TOKEN]
+    if token.present?
+      u = User.find_by(login_token: token)
+      @current_resource_owner = u if u.present?
+    end
+
+    if token.blank? || @current_resource_owner.blank?
+      render json: { error_message: "再ログインが必要です" }
+    end
+  end
+
 end
